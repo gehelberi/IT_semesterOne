01430b5e699a41502d001f4600eed5ceb730784d
gehelberi <59255187+gehelberi@users.noreply.github.com>
2021-05-17 14:57:37 +0300

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,177 @@
+import React, {Component} from 'react';
+
+class Home extends React.Component{
+render(){
+return(
+<div>
+<header>
+<div id="logo">
+<a href="#top"><img class="image" src="assets/img/IronTech.svg" width="200" height="200" align="left" href="#top"/></a>
+</div>
+<div id="about">
+<a href="#main" title="Преимущества">Преимущества</a>
+<a href="#overview" title="Услуги">Услуги</a>
+<a href="#contacts" title="Контакты">Контакты</a>
+<a href="#faq" title="Ответы на вопросы">FAQ</a>
+</div>
+</header>
+
+<div id="top">
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<h3>Ремонт компьютеров и ноутбуков </h3>
+<h3>в Минске</h3>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p><strong>ВДОХНЕМ ЖИЗНЬ</strong><strong><br/>
+В ВАШ ДЕВАЙС УЖЕ СЕГОДНЯ!</strong></p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<font size="5">
+<h3>*первое обращение – скидка 20%<br/>
+*бесплатная диагностика</h3>
+</font>
+<p>&nbsp;</p>
+</div>
+
+<div id="main">
+<div class="intro">
+<p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+<span>Помощь в подборе запчастей и программ</span>
+</div>
+<div class="text">
+<img src="assets/img/shield.svg" width="250" height="250" align="right" alt=""/>
+</div>
+<p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+<div class="text">
+<img src="assets/img/guarantee.svg" width="250" height="250" align="left" alt=""/>
+</div>
+<div class="intro">
+<p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+<span>Гарантия от мастера до 3-х лет</span>
+</div>
+<p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+<div class="intro">
+<p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+<span>Выезд мастера на адрес в течение 30-ти минут</span>
+</div>
+<div class="text">
+<img src="assets/img/car.svg" width="250" height="250" align="right" alt=""/>
+</div>
+</div>
+
+<div id="overview">
+<h2><strong>Услуги</strong></h2>
+<h4>Мастера работают на качество и результат, дорожат каждым обратившимся клиентом.</h4>
+<div class="img">
+<img src="assets/img/1.jpg" width="425" height="280" alt=""/>
+<span>Установка программ и обновлений</span>
+<p>&nbsp;</p>
+<p>Мастер установит продукты Adobe, Microsoft Office, браузер и любую другую программу! Настроит и установит обновления. Просто оставьте свою заявку!</p>
+</div>
+<div class="img">
+<img src="assets/img/2.jpg" width="425" height="280" alt=""/>
+<span>Установка Microsoft Windows/OS Linux</span>
+<p>&nbsp;</p>
+<p>Мастер установит Windows XP, Windows 7, Windows 8, Windows 10 или OS Linux. Быстро и качественно. Все ваши данные будут сохранены. Доверьте работу профессионалу!</p>
+</div>
+<div class="img">
+<img src="assets/img/3.jpg" width="425" height="280" alt=""/>
+<span>Удаление вирусов</span>
+<p>&nbsp;</p>
+<p>Вирус повредил Ваш компьютер? Ваши файлы стали зашифрованными? Баннер заблокировал вход в систему? Реклама лезет со всех сторон? Не отчаивайтесь! Мастер очистит Ваш компьютер!</p>
+</div>
+<div class="img">
+<img src="assets/img/4.jpg" width="425" height="280" alt=""/>
+<span>Чистка от пыли</span>
+<p>&nbsp;</p>
+<p>Ваше устройство греется? Чистка от пыли и грязи, замена кулера, замена системы охлаждения, замена термопасты. Все ремонтные работы выполняются у Вас на глазах!</p>
+</div>
+<div class="img">
+<img src="assets/img/5.jpg" width="425" height="280" alt=""/>
+<span>Замена комплектующих</span>
+<p>&nbsp;</p>
+<p>Аппарат не включается? Мастер найдет причину! Поменяет любую деталь: замена клавиатуры, зарядного устройства, матрицы, видеокарты, материнской платы. Только профессионалы!</p>
+</div>
+<div class="img">
+<img src="assets/img/6.jpg" width="425" height="280" alt=""/>
+<span>Настройка Интернета</span>
+<p>&nbsp;</p>
+<p>Пропал доступ в Интернет? Надо настроить Wi-Fi роутер? Компьютер не подключается к Wi-Fi сети? Мастера решат с этим проблемы за считанные минуты. Не тратьте напрасно свое время, просто оставьте свою
+заявку!</p>
+</div>
+<div class="img">
+<img src="assets/img/7.png" width="425" height="280" alt=""/>
+<span>Восстановление данных</span>
+<p>&nbsp;</p>
+<p>Мастера помогут восстановить информацию из-за повреждений механического характера,
+ 
+форматирования, проблем в файловых системах утопленные, с поврежденными контактами, после воздействия электрического разряда.</p>
+</div>
+<div class="img">
+<img src="assets/img/8.jpg" width="425" height="280" alt=""/>
+<span>Ремонт, замена матрицы</span>
+<p>&nbsp;</p>
+<p>Сотрудничество мастеров с партнерскими сервисами существенно расширяет географию работы и облегчает поиск редких комплектующих, поэтому мастер может произвести замену матрицы практически любых современных популярных брендов-производителей.</p>
+</div>
+</div>
+
+<div id="contacts">
+<center>
+<h5><strong>Уже знаете, что не так?</strong></h5>
+</center>
+<form class="decor">
+<div class="form-left-decoration"></div>
+<div class="form-right-decoration"></div>
+<div class="circle"></div>
+<div class="form-inner">
+<h3><center>Получить консультацию и скидку:</center></h3>
+<p>&nbsp;</p>
+<input type="text" placeholder="Введите имя" name="name" id="name"/>
+<input type="phone" placeholder="+375 (__) ___-__-__" data-mask="+375 (00) 000-00-00" pattern="([0-9]{2}) [0-9]{4}[-][0-9]{4}" name="phone" id="tel"/>
+<textarea placeholder="Опишите характер проблемы..." rows="3"></textarea>
+<input type="submit" value="Отправить"/>
+</div>
+</form>
+</div>
+
+<div id="faq">
+<div>
+<span class="title">Как мы работаем:</span><br/>
+<p>&nbsp;</p>
+<span class="heading"><img src="assets/img/describe.svg" width="50" height="50" class="leftimg"/> &nbsp;1. Заказ</span>
+<p>Вы делаете заказ по телефону или онлайн через наш сайт.</p>
+<span class="heading"><img src="assets/img/describe.svg" width="50" height="50" class="leftimg"/> &nbsp;2. Диагностика</span>
+<p>В срок от 30 минут мастер приезжает к вам и производит диагностику. Если Вас устраивает стоимость услуг, мы приступаем к работам.</p>
+<span class="heading"><img src="assets/img/describe.svg" width="50" height="50" class="leftimg"/> &nbsp;3. Исправная техника</span>
+<p>По окончании работ вы получаете исправную технику и бланк БСО.</p>
+</div>
+<div>
+<span class="title">Почему мы?</span><br/>
+<p>&nbsp;</p>
+<span class="heading"><img src="assets/img/gear.svg" width="50" height="50" class="leftimg"/> &nbsp;Услуги для юр. лиц</span>
+<p>Наш центр обслуживает юридические лица и государственные учреждения.</p>
+<span class="heading"><img src="assets/img/gear.svg" width="50" height="50" class="leftimg"/> &nbsp;Заправка картриджей</span>
+<p>Осуществляем заправку картриджей, расходные материалы подбираем индивидуально.</p>
+<span class="heading"><img src="assets/img/gear.svg" width="50" height="50" class="leftimg"/> &nbsp;Система скидок %</span>
+<p>Делаем различные скидки постоянным клиентам, разрабатываем выгодные предложения.</p>
+</div>
+<div>
+<span class="title">Гарантии</span><br/>
+<p>&nbsp;</p>
+<span class="heading"><img src="assets/img/support.svg" width="50" height="50" class="leftimg"/> &nbsp;Лучшие специалисты</span>
+<p>Мастера с многолетним стажем и профильным образованием решат любые программные и аппаратные проблемы вашего компьютера</p>
+<span class="heading"><img src="assets/img/support.svg" width="50" height="50" class="leftimg"/> &nbsp;Гарантия на услуги</span>
+<p>Мастер гарантирует качество ремонта ПК, сведя к минимуму количество повторных обращений</p>
+<span class="heading"><img src="assets/img/support.svg" width="50" height="50" class="leftimg"/> &nbsp;Оперативность</span>
+<p>В течение получаса после получения заявки мастер выезжает домой или в офис, приступает к ремонту немедленно, дает гарантию на все виды работ</p>
+</div>
+</div>
+</div>
+)
+}
+}
+export default Home
